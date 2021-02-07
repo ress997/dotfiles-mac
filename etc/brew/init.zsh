151611a08957d03b9feba8b391f97821ab5f2c50
Ress <48500247+ress997@users.noreply.github.com>
2021-02-07 22:30:45 +0900

    update kitty settings

---
+++ etc/brew/init.zsh
@@ -0,0 +1,38 @@
+#!/usr/bin/env zsh
+
+brew install go
+brew install rust
+
+## C
+brew install fzy
+## Go
+brew install fzf
+brew install ghq
+brew install hugo
+## Rust
+brew install bat
+brew install fd
+brew install ripgrep
+brew install sd
+
+# Miss
+brew install tig
+
+brew install neovim
+# Build
+#brew install luajit --HEAD
+#brew install tree-sitter --HEAD
+#brew install neovim --HEAD
+# https://github.com/neovim/pynvim をインストール
+#brew install python
+#pip3 install -U pynvim
+
+brew tap homebrew/cask
+# Quick Look
+brew install qlimagesize
+brew install qlmarkdown
+brew install webpquicklook
+
+# Font
+brew tap homebrew/cask-fonts
+brew install font-cica
